64763ad9f03df2558e83bdb62368841cef29ab26
Jason Jordan <jasonajordan0@gmail.com>
2020-10-04 02:54:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,10 +38,10 @@ def game_hash
   },
   :away =>  { :team_name => "Charlotte Hornets",
               :colors => ['Turquoise', 'Purple'],
-              :players => [{:player_name => 'Alan Anderson',
-                          :number => 0, :shoe => 16, :points => 22,
-                          :rebounds => 12, :assists => 12, :steals => 3,
-                          :blocks => 1,:slam_dunks => 1
+              :players => [{:player_name => 'Jeff Adrien',
+                          :number => 4, :shoe => 18, :points => 10,
+                          :rebounds => 1, :assists => 11, :steals => 2,
+                          :blocks => 7,:slam_dunks => 2
                           },
                           
                           {:player_name => 'Reggie Evans',
