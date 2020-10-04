97d22776c4fbb1307b7a2c2062dce48f0ccac2b8
Jason Jordan <jasonajordan0@gmail.com>
2020-10-04 02:08:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,11 @@
 def game_hash
   
   {:home => { :team_name => "Brooklyn Nets",
-              :colors => []
+              :colors => ['Black', 'White'],
               :players =>
   },
   :away =>  { :team_name => "Charlotte Hornets",
-              :colors => 
+              :colors => []
               :players =>
   }
   }
