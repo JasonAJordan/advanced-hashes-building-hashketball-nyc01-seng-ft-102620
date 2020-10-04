abc85238a063f866ce265b75781d416247a0124f
Jason Jordan <jasonajordan0@gmail.com>
2020-10-04 02:34:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,24 +4,23 @@ def game_hash
   
   hash = {:home => { :team_name => "Brooklyn Nets",
               :colors => ['Black', 'White'],
-              :players => [:alan_anderson => {:player_name => 'Alan Anderson',
+              :players => [{:alan_anderson => {:player_name => 'Alan Anderson',
                           :number => 0, :shoe => 16, :points => 22,
                           :rebounds => 12, :assists => 12, :steals => 3,
                           :blocks => 1,:slam_dunks => 1
-                          }
-                          ,
+                          }},
                           
-                          :reggie_evans => {:player_name => 'Reggie Evans',
+                          {:reggie_evans => {:player_name => 'Reggie Evans',
                           :number => 30, :shoe => 14, :points => 12,
                           :rebounds => 12, :assists => 12, :steals => 12,
                           :blocks => 12, :slam_dunks => 7
-                          },
+                          }},
                           
-                          :brook_lopez => {:player_name => 'Brook Lopez',
+                          {:brook_lopez => {:player_name => 'Brook Lopez',
                           :number => 11, :shoe => 17, :points => 17,
                           :rebounds => 19, :assists => 10, :steals => 3,
                           :blocks => 1, :slam_dunks => 15
-                          }
+                          }}
                           ]
   },
   :away =>  { :team_name => "Charlotte Hornets",
