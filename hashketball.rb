bd4bc7daddf47551a6d2fe499e4bce122a1e8149
Jason Jordan <jasonajordan0@gmail.com>
2020-10-04 02:51:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,29 +10,29 @@ def game_hash
                           :blocks => 1,:slam_dunks => 1
                           },
                           
-                          # {:reggie_evans => {:player_name => 'Reggie Evans',
-                          # :number => 30, :shoe => 14, :points => 12,
-                          # :rebounds => 12, :assists => 12, :steals => 12,
-                          # :blocks => 12, :slam_dunks => 7
-                          # }},
+                          {:player_name => 'Reggie Evans',
+                          :number => 30, :shoe => 14, :points => 12,
+                          :rebounds => 12, :assists => 12, :steals => 12,
+                          :blocks => 12, :slam_dunks => 7
+                          },
                           
-                          # {:brook_lopez => {:player_name => 'Brook Lopez',
-                          # :number => 11, :shoe => 17, :points => 17,
-                          # :rebounds => 19, :assists => 10, :steals => 3,
-                          # :blocks => 1, :slam_dunks => 15
-                          # }},
+                          {:player_name => 'Brook Lopez',
+                          :number => 11, :shoe => 17, :points => 17,
+                          :rebounds => 19, :assists => 10, :steals => 3,
+                          :blocks => 1, :slam_dunks => 15
+                          },
                           
-                          # {:mason_plumlee => {:player_name => 'Mason Plumlee',
-                          # :number => 1, :shoe => 19, :points => 26,
-                          # :rebounds => 11, :assists => 6, :steals => 3,
-                          # :blocks => 8, :slam_dunks => 5
-                          # }},
+                          {:mason_plumlee => {:player_name => 'Mason Plumlee',
+                          :number => 1, :shoe => 19, :points => 26,
+                          :rebounds => 11, :assists => 6, :steals => 3,
+                          :blocks => 8, :slam_dunks => 5
+                          },
                           
-                          # {:jason_terry => {:player_name => 'Jason Terry',
-                          # :number => 31, :shoe => 15, :points => 19,
-                          # :rebounds => 2, :assists => 2, :steals => 4,
-                          # :blocks => 11, :slam_dunks => 1
-                          # }}
+                          {:player_name => 'Jason Terry',
+                          :number => 31, :shoe => 15, :points => 19,
+                          :rebounds => 2, :assists => 2, :steals => 4,
+                          :blocks => 11, :slam_dunks => 1
+                          }
                           ]
   },
   :away =>  { :team_name => "Charlotte Hornets",
